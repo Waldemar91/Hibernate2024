5b43c8ab8ce308013acfbbd236beaf000e4761d9
Waldemar91 <volodymyr.shenbor@gmail.com>
2024-01-23 09:21:24 +0200

    Java Lesson6

--- src/main/java/hibernate/Product.java
+++ src/main/java/hibernate/Product.java
@@ -1,2 +1,22 @@
-package hibernate;public class Product {
+package hibernate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products", schema = "public")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
 }
